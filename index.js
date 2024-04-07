801cd21de980143792140c71f7e1bd185a893ddb
user.name <akamrannicole@gmail.com>
2024-04-07 11:46:01 +0300

    js events lab done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// Wrap the code in a function as instructed
 function addingEventListener() {
+    const input = document.getElementById('button');
+    
+    // Add an event listener for click event
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
+// Call the function to activate the event listener
+addingEventListener();
